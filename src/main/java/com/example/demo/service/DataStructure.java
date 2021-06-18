f6b3990b603484cbdf97991b5a48a4ad17ab196e
donggi <ldg6153@gmail.com>
2021-06-18 23:03:31 +0900

    Add TreeSet test5

--- src/main/java/com/example/demo/service/DataStructure.java
+++ src/main/java/com/example/demo/service/DataStructure.java
@@ -302,6 +302,8 @@ public class DataStructure {
         //subset 범위검색의 결과를 반환한다
         //headset 지정된 객체보다 작은 값의 객체들을 반환한다
         //tailset지정된 객체보다 큰 값의 객체들을 반환한다
+        //트리 순회
+        // 이진 트리의 모든 노드를 한번씩 읽는 것을 트리 순회라고 한다.
     }
     class TreeSetTest {}
 
