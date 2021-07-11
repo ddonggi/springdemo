5febe68f99eccadad213f4c2db944bb774278177
donggi <ldg6153@gmail.com>
2021-07-11 22:56:57 +0900

    Add hashmap

--- src/main/java/com/example/demo/service/DataStructure.java
+++ src/main/java/com/example/demo/service/DataStructure.java
@@ -681,6 +681,20 @@ public class DataStructure {
             System.out.println("최고점:"+Collections.max(values));
             System.out.println("최저점:"+Collections.min(values));
 
+            String[] data = {"A","K","A","K","D","K","K","K","Z","D"};
+
+            HashMap map2 = new HashMap();
+
+            for(int i =0; i<data.length; i++){
+                if(map2.containsKey(data[i])){
+                    int value = (int)map.get(data[i]);
+                    map.put(data[i],value+1);
+                }
+                else{
+                    map.put(data[i],1);
+                }
+            }
+
         }//main
     }
 
